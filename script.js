3a7ea3428cb1b3732892ae2931d0542acdbfd4f3
Lindsay Urchyk <urchykli@gmail.com>
2019-03-26 13:18:05 -0400

    links opening in new tabs and one img showing on hover

---
+++ script.js
@@ -0,0 +1,23 @@
+function gitTab () {
+	window.open('https://github.com/urchykli', '_blank')
+}
+
+function aboutTab () {
+	window.open('./about.html', '_blank')
+}
+
+function resumeTab () {
+	window.open('./LINDSAYURCHYK_RESUME.pdf', '_blank')
+}
+function dogTab () {
+	window.open('http://dog-park-finder.surge.sh/', '_blank')
+}
+function breweryTab () {
+	window.open('http://road-soda.surge.sh/', '_blank')
+}
+function petTab () {
+	window.open('https://petitlku.herokuapp.com/', '_blank')
+}
+function hangmanTab () {
+	window.open('https://urchykli.github.io/hangman-project/', '_blank')
+}
